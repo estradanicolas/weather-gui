371b56c56e54cea14c90e0935d03365fe690d8bf
Nicolas Estrada <59927633+estradanicolas@users.noreply.github.com>
2020-04-02 07:53:38 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+import requests
+import json
+from pprint import pprint
+
+HEIGHT = 700
+WIDTH = 800
+
+
+def format_response(weather):
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+
+        final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (name, desc, temp)
+    except:
+        final_str = 'There was a problem retrieving the weather information.'
+
+    return final_str
+
+def get_weather(city):
+    #api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+    weather_key = '7f80c772990d914cca00862648e365d7'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    weather = response.json()
+    label['text'] = format_response(weather)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='sky.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#FFCCCC', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor = 'n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda:get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame =tk.Frame(root, bg='#FFCCCC', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+root.mainloop()
